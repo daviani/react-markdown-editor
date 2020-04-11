646dd55a93e28a110233ef8024e8a0b47c9c9d67
Dav <d.fillatre81@gmail.com>
2020-04-11 13:33:39 +0200

    mise en place de la lib markedJS et de la sauvegarde sur localstorage \n ⚠️  soucis avec méthode sanitaze(deprecated)

--- src/App.js
+++ src/App.js
@@ -1,19 +1,63 @@
 import React, { Component } from 'react'
 import './App.css'
+import { sampleText } from './sampleText'
+import marked from 'marked'
+//import DOMPurify from 'dompurify'
+
 
 class App extends Component {
-  render () {
+  state = {
+    text: sampleText
+  }
+
+  componentDidMount() {
+    const text = localStorage.getItem('text')
+
+    if (text) {
+      this.setState({ text })
+    } else {
+      this.setState({ text:sampleText })
+    }
+  }
+
+  componentDidUpdate() {
+    const { text } = this.state
+    localStorage.setItem('text', text)
+  }
+
+  handleChange = event => {
+    const text = event.target.value
+    this.setState({ text })
+  }
+
+  renderText = text => {
+    //TODO Voir la libs DOMPurify pour remplacer sanatize qui est déprécier
+    /*const config = {
+      ALLOWED_TAGS: ['p']
+    }
+    const __html = marked(DOMPurify.sanitize(text, config))
+    return { __html }*/
+    const __html = marked(text, { sanitize: true })
+    return { __html }
+
+  }
+
+  render() {
     return (
       <div className='container'>
         <div className='row'>
           <div className='col-sm-6'>
             <textarea
+              onChange={this.handleChange}
+              value={this.state.text}
               className='form-control'
               rows='35'
             />
           </div>
           <div className='col-sm-6'>
-            <h1>Résultat :</h1>
+            <div
+              dangerouslySetInnerHTML={this.renderText(this.state.text)}
+            />
           </div>
         </div>
       </div>
